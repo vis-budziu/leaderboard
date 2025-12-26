97172ae43d3a344e8ed6840f181b6827a0ff95c7
NedasGulb <nedas.gulbickas@meritstory.com>
2025-12-26 11:19:40 +0200

    Initial Initial commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,5 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+import Leaderboard from "~/leaderboard";
 
 export default function Home() {
-  return <Welcome />;
+  return <Leaderboard />;
 }
